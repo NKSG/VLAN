2abfd4ee9df34122d4f77a59fd42dececd661e21
kapadiamush <kapadiamush@gmail.com>
2013-10-05 17:13:06 -0400

    Simple set up stuff for the project

---
+++ proxy.h
@@ -0,0 +1,14 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+
+#define true 0
+#define false 1
+
+typedef int boolean; 
