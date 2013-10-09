95effe28947c1b739aba2293611f9680851f150d
Kevin Hsieh <hsieh63@scarletmail.rutgers.edu>
2013-10-08 23:54:49 -0400

    Edited proxy.c. Added code from simpletun.c and the project pdf. Also added comments to change in file

--- proxy.c
+++ proxy.c
@@ -1,4 +1,27 @@
 #include "proxy.h"
+//from simpletun c slide
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <linux/if_tun.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <stdarg.h>
+
+/* buffer for reading from tun/tap interface, must be >= 1500 */
+#define BUFSIZE 2000   
+#define CLIENT 0
+#define SERVER 1
+#define PORT 55555
 
 int main(int argc, char **argv){
 	if(argc == 1 && (strcmp(argv[1], "-h"))){
@@ -21,6 +44,84 @@ int printHelp(){
 	printf("If setting up for client use:\n\t./proxy <remote host> <remote port> <local interface>\n"); 
 	printf("\nThank you for using our proxy!\n"); 
 }
+
+/**************************************************
+* allocate_tunnel:
+* open a tun or tap device and returns the file
+* descriptor to read/write back to the caller
+* from project pdf
+*****************************************/
+int allocate_tunnel(char *dev, int flags) {
+    int fd, error;
+    struct ifreq ifr;
+    char *device_name = "/dev/net/tun";
+    if( (fd = open(device_name , O_RDWR)) < 0 ) {
+      perror("error opening /dev/net/tun");
+      return fd;
+    }
+    memset(&ifr, 0, sizeof(ifr));
+    ifr.ifr_flags = flags;
+    if (*dev) {
+        strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+    }
+    if( (error = ioctl(fd, TUNSETIFF, (void *)&ifr)) < 0 ) {
+      perror("ioctl on tap failed");
+      close(fd);
+      return error;
+    }
+    strcpy(dev, ifr.ifr_name);
+    return fd;
+}
+
+/**************************************************************************
+ * cread: read routine that checks for errors and exits if an error is    *
+  *        returned.                                                       *
+   **************************************************************************/
+int cread(int fd, char *buf, int n){
+
+    int nread;
+
+    if((nread=read(fd, buf, n)) < 0){
+        perror("Reading data");
+        exit(1);
+    }
+    return nread;
+}
+
+/**************************************************************************
+* cwrite: write routine that checks for errors and exits if an error is  *
+*         returned.                                                      *
+**************************************************************************/
+int cwrite(int fd, char *buf, int n){
+
+    int nwrite;
+
+    if((nwrite=write(fd, buf, n)) < 0){
+        perror("Writing data");
+        exit(1);
+    }
+    return nwrite;
+}
+
+/**************************************************************************
+* read_n: ensures we read exactly n bytes, and puts them into "buf".     *
+*         (unless EOF, of course)                                        *
+**************************************************************************/
+int read_n(int fd, char *buf, int n) {
+
+    int nread, left = n;
+
+    while(left > 0) {
+        if ((nread = cread(fd, buf, left)) == 0){
+            return 0 ;
+        }else {
+            left -= nread;
+            buf += nread;
+        }
+    }
+    return n;
+}
+
 /*
  * reads from the tap device and writes to the port
  */
@@ -30,6 +131,8 @@ int Client(char **argv){
 	char buffer[2024]; 
 	boolean binder, listener; 
 	int host, port, reader; 
+    //tun variables
+    int maxfd;
 	
 	scanf(argv[2], "%d", port); //gets the port from ascii to int
 
@@ -59,15 +162,80 @@ int Client(char **argv){
 		return 1; 
 	}
 
-	while(true){
+    /* Connecting to tap. now you can read/write on tap_fd (used combo of tunnel function 
+        from assignment pdf + simpletun c file */ 
+    char *if_name = “tap0”;
+    if ( (tap_fd = allocate_tunnel(if_name, IFF_TAP | IFF_NO_PI)) < 0 ) {
+        perror("Opening tap interface failed! \n");
+        exit(1);
+    }
 
-		/* have to figure out how to read from the tap
-		 * device and write out to the socket. 
-		 */
-	}
-	
+    /* use select() to handle two descriptors at once */
+    maxfd = (tap_fd > net_fd)?tap_fd:net_fd;
 
+    while(1){
+
+        /* have to figure out how to read from the tap
+        * device and write out to the socket. 
+        * Copied direct from simpletun file 
+        */
+        int ret;
+        fd_set rd_set;
+
+        FD_ZERO(&rd_set);
+        FD_SET(tap_fd, &rd_set); FD_SET(net_fd, &rd_set);
+
+        ret = select(maxfd + 1, &rd_set, NULL, NULL, NULL);
+
+        if (ret < 0 && errno == EINTR){
+            continue;
+        }
+
+        if (ret < 0) {
+            perror("select()");
+            exit(1);
+        }
+
+        if(FD_ISSET(tap_fd, &rd_set)) {
+            /* data from tun/tap: just read it and write it to the network */
+
+            nread = cread(tap_fd, buffer, BUFSIZE);
+
+            tap2net++;
+            do_debug("TAP2NET %lu: Read %d bytes from the tap interface\n", tap2net, nread);
+
+            /* write length + packet */
+            plength = htons(nread);
+            nwrite = cwrite(net_fd, (char *)&plength, sizeof(plength));
+            nwrite = cwrite(net_fd, buffer, nread);
+
+            do_debug("TAP2NET %lu: Written %d bytes to the network\n", tap2net, nwrite);
+        }
+
+        if(FD_ISSET(net_fd, &rd_set)) {
+            /* data from the network: read it, and write it to the tun/tap interface. 
+            * We need to read the length first, and then the packet */
+
+            /* Read length */
+            nread = read_n(net_fd, (char *)&plength, sizeof(plength));
+            if(nread == 0) {
+                /* ctrl-c at the other end */
+                break;
+            }
+
+            net2tap++;
+
+            /* read packet */
+            nread = read_n(net_fd, buffer, ntohs(plength));
+            do_debug("NET2TAP %lu: Read %d bytes from the network\n", net2tap, nread);
+
+            /* now buffer[] contains a full packet or frame, write it into the tun/tap interface */
+            nwrite = cwrite(tap_fd, buffer, nread);
+            do_debug("NET2TAP %lu: Written %d bytes to the tap interface\n", net2tap, nwrite);
+        }
+    }
 }
+
 /*
  * reads from the port and writes to the tap device
  */
